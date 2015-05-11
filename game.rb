12ae1138ec7ad7411f82475671820d7c6a4db4d3
Amie Wilt <amie.wilt@gmail.com>
2015-05-11 16:29:06 -0400

    implemented the player and game classes to get remaining tests passing

--- game.rb
+++ game.rb
@@ -1,9 +1,38 @@
 require "./dice"
 
 class Player
+
+  attr_reader :dice
+
+  def initialize(name, dice)
+    @dice = dice
+  end
 end
 
-
 class Game
-  # Players roll their dice until someone wins
+
+  attr_reader :turn, :winner
+
+  def initialize(player_1, player_2)
+    @player_1 = player_1
+    @player_2 = player_2
+    @turn = 0
+    @winner = winner
+  end
+
+  def play!
+    
+    until @winner
+      player_1_roll = @player_1.dice.roll
+      player_2_roll = @player_2.dice.roll
+
+      @turn += 1
+
+      if player_1_roll > player_2_roll
+        @winner = @player_1
+      elsif player_1_roll <  player_2_roll
+        @winner = @player_2
+      end
+    end
+  end
 end
